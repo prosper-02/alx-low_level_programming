75cb5fbba97dd889a5015fae7e58b40fcae7e39f
wambuaprosperkavengulia <kavenguliaprosper>
2023-06-15 03:09:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - 4-puts.c
- * description - Write a C program that prints exactly
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
