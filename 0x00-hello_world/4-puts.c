b011b4dc7b89e1a8383d2b0b2df4735a6bf3a9c8
wambuaprosperkavengulia <kavenguliaprosper>
2023-06-15 02:54:32 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/** 
+ * main -4-puts.c
+ * discription -write a program that prints exactly
+ *
+ * return: always 0 (sucess)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+
+        return (0);
+}
+
