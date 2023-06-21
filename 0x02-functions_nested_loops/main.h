79ae8f517c94ffc088310f3035ac00654a1af2d3
wambuaprosperkavengulia <kavenguliaprosper>
2023-06-21 11:58:49 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,1 @@
-int _putchar(char c);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-void print_alphabet_x10(void);
+void print_alphabet(void);
